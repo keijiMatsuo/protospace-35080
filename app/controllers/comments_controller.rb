f02a6052a9c14b2173a66804c1b81745edee19af
ojyussan <matsuoexpress@gmail.com>
2021-04-08 14:55:32 +0900

    Revert "second commit"
    This reverts commit 755469a8e5d6a2f261df25d778a7b8cb11a35455.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,6 @@ class CommentsController < ApplicationController
   end
 
   private
-
   def comment_params
     params.require(:comment).permit(:text)
     .merge(user_id: current_user.id, prototype_id: params[:prototype_id])
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,10 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show, :new]
+  before_action :move_to_index,      only: [:edit, :update, :destroy]
+  before_action :set_prototype,      only: [:show, :edit, :update, :destroy]
+
   def index
-    @prototypes= Prototype.includes(:user)
+    @prototypes= Prototype.all
   end
 
   def new
@@ -10,7 +12,7 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    @prototype= Prototype.new(prototype_params)
+    @prototype= Prototype.create(prototype_params)
     if @prototype.save
       redirect_to root_path
     else
@@ -20,38 +22,34 @@ class PrototypesController < ApplicationController
 
   def show
     @comment=   Comment.new
-    @comments=  @prototype.comments
+    @comments=  @prototype.comments.includes(:user)
   end
 
   def edit
-    if current_user.id == @prototype.user.id
-      render :edit
-    else
-      redirect_to root_path
-    end
   end
 
   def update
-    if @prototype.update(prototype_params)
-      redirect_to prototype_path(@prototype)
+    @prototype.update(prototype_params)
+    if @prototype.save
+      redirect_to prototype_path
     else
       render :edit
     end
   end
 
   def destroy
-    if @prototype.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
+    @prototype.destroy
+    redirect_to root_path
   end
 
   private
-
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
     .merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user== @prototype
   end
 
   def set_prototype
